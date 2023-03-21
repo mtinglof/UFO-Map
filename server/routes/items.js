de47fad51b5f320be22fb6733210ce1d4ec22d3b
Michael Tinglof <mike.tinglof@gmail.com>
2023-03-21 13:26:19 -0700

    Setup server file for retriving all route options for our data type 'item'

---
+++ server/routes/items.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/Item');
+
+// Route for getting all items
+router.get('/', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Route for getting a single item by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    res.json(item);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Route for creating a new item
+router.post('/', async (req, res) => {
+  const item = new Item({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+  });
+  try {
+    const newItem = await item.save();
+    res.status(201).json(newItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Route for updating an existing item
+router.patch('/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    if (req.body.name) {
+      item.name = req.body.name;
+    }
+    if (req.body.description) {
+      item.description = req.body.description;
+    }
+    if (req.body.price) {
+      item.price = req.body.price;
+    }
+    const updatedItem = await item.save();
+    res.json(updatedItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Route for deleting an item
+router.delete('/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    await item.remove();
+    res.json({ message: 'Item deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const itemsRouter = require('./routes/items');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const app = express();
 
-app.listen(3000, () =>{
-    console.log('Server started on port 3000')
+// Connect to MongoDB database
+mongoose.connect('mongodb://localhost/my-database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+// Load routes
+app.use('/api/items', itemsRouter);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
